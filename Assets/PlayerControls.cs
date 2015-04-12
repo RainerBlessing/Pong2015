af6ff2d4312f10b7fd0395bb1d37f0f396536430
Rainer Blessing <rainer.blessing@googlemail.com>
2015-04-12 20:52:56 +0200

    changed PlayerControls to c#

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class PlayerControls : MonoBehaviour {
+
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+	
+	float speed = 10;
+	
+	int startPositionX;
+	
+	void  SetStartPosition ( int position  ){
+		startPositionX = position;
+	}
+	void  Update (){
+		Rigidbody2D playerRigidBody2D = GetComponent<Rigidbody2D> ();
+
+		if(Input.GetKey(moveUp)){
+			playerRigidBody2D.velocity = new Vector2(0,speed);		
+		}
+		else if (Input.GetKey(moveDown)){
+			playerRigidBody2D.velocity = new Vector2(0,speed * -1);
+		}else{
+			playerRigidBody2D.velocity = new Vector2(0,0);
+		}
+	}
+	
+	void  Reset (){
+		GetComponent<Rigidbody2D>().transform.position = new Vector2(0,0);	
+	}
+}
+
